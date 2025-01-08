701ceff091cd78ad35fa1f73959743904daff2ba
Daniil Polienko <donyaschefer@gmail.com>
2025-01-08 16:32:21 +0300

    fix: eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import type {ReactNode} from 'react';
+
 import './globals.css';
 
 export default function RootLayout({
 	children
 }: Readonly<{
 	children: React.ReactNode;
-}>) {
+}>): ReactNode {
 	return (
-		<html lang="en">
+		<html lang={'en'}>
 			<body>{children}</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import type {ReactElement} from 'react';
+
 import {Button} from '@/components/Button';
 
-export default function Home() {
+export default function Home(): ReactElement {
 	return (
-		<div className="font-[family-name:var(--font-geist-sans)]">
-			<h1 className="text-[#cccccc] font-bold text-white text-black">Hello World</h1>
+		<div className={'font-[family-name:var(--font-geist-sans)]'}>
+			<h1 className={'font-bold text-white'}>{'Hello World'}</h1>
 			<Button />
 		</div>
 	);
