cee154f015be49202868eddbd6be9149826a6c82
Daniil Polienko <donyaschefer@gmail.com>
2025-02-07 22:19:43 +0300

    fix: weglot attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import {NextIntlClientProvider} from 'next-intl';
 import {getLocale, getMessages} from 'next-intl/server';
 
@@ -14,6 +15,20 @@ export default async function RootLayout({children}: {children: ReactNode}): Pro
 
 	return (
 		<html lang={locale}>
+			<head>
+				<Script
+					strategy={'beforeInteractive'}
+					type={'text/javascript'}
+					src={'https://cdn.weglot.com/weglot.min.js'}
+					crossOrigin={'anonymous'}
+				/>
+				<Script
+					strategy={'afterInteractive'}
+					id={'weglot'}
+					crossOrigin={'anonymous'}>
+					{"Weglot.initialize({api_key: 'wg_b6fdc2a2e16175fd09ce44998516155b3'});"}
+				</Script>
+			</head>
 			<body className={'relative min-h-screen overflow-x-hidden bg-bg text-white'}>
 				<WithFonts>
 					<NextIntlClientProvider messages={messages}>
