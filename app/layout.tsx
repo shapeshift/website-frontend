db96c82155989c018bccc4ed6e569a46ed2cf918
Daniil Polienko <donyaschefer@gmail.com>
2025-02-07 20:01:24 +0300

    feat: add weglot script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import {NextIntlClientProvider} from 'next-intl';
 import {getLocale, getMessages} from 'next-intl/server';
 
@@ -14,6 +15,29 @@ export default async function RootLayout({children}: {children: ReactNode}): Pro
 
 	return (
 		<html lang={locale}>
+			<link
+				rel={'alternate'}
+				hrefLang={'en'}
+				href={'https://shapeshift.builtby.dad'}
+			/>
+			<link
+				rel={'alternate'}
+				hrefLang={'fr'}
+				href={'https://fr.shapeshift.builtby.dad'}
+			/>
+			<Script
+				type={'text/javascript'}
+				src={'https://cdn.weglot.com/weglot.min.js'}
+			/>
+			<Script
+				id={'weglot'}
+				dangerouslySetInnerHTML={{
+					// eslint-disable-next-line @typescript-eslint/naming-convention
+					__html: ` Weglot.initialize({
+        api_key: 'wg_d200799f995ece1df7fb54af8c7397f82'
+    });`
+				}}
+			/>
 			<body className={'relative min-h-screen overflow-x-hidden bg-bg text-white'}>
 				<WithFonts>
 					<NextIntlClientProvider messages={messages}>
