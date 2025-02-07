b612a93f3a308a5ce25a54107d06beba4bfd322e
Daniil Polienko <donyaschefer@gmail.com>
2025-02-07 19:24:03 +0300

    chore: try adding lib

--- app/layout.client.tsx
+++ app/layout.client.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {usePathname} from 'next/navigation';
+import {useWeglot} from 'react-weglot';
 
 import {Footer} from '@/components/Footer';
 import {Header} from '@/components/header/Header';
@@ -13,10 +14,13 @@ import type {ReactNode} from 'react';
  ********************************************************************************************/
 export function LayoutClient({children}: {children: ReactNode}): ReactNode {
 	const pathname = usePathname();
+	const [lang, setLang] = useWeglot('wg_d200799f995ece1df7fb54af8c7397f82', 'en');
 
 	return (
 		<div className={'flex flex-col px-4'}>
 			{pathname === '/' ? null : <Header />}
+			<button onClick={() => setLang('en')}>{'EN'}</button>
+			<button onClick={() => setLang('fr')}>{'FR'}</button>
 			<main>{children}</main>
 			<Footer />
 		</div>
