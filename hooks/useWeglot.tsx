6d37ac3fc606f0ed81b35fd73883403bd3fc9524
Daniil Polienko <donyaschefer@gmail.com>
2025-02-10 22:46:36 +0300

    fix: attempt

--- hooks/useWeglot.tsx
+++ hooks/useWeglot.tsx
@@ -33,14 +33,14 @@ export const useWeglot = (apiKey: string, defaultLanguage: string = 'en'): [stri
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	useEffect(() => {
-		// @ts-ignore
-		if (window.Weglot.initialized) {
-			// @ts-ignore
-			window.Weglot.switchTo(language);
-			console.log('switched to', language);
-		}
-	}, [language]);
+	// useEffect(() => {
+	// 	// @ts-ignore
+	// 	if (window.Weglot.initialized) {
+	// 		// @ts-ignore
+	// 		window.Weglot.switchTo(language);
+	// 		console.log('switched to', language);
+	// 	}
+	// }, [language]);
 
 	return [language, setLanguage] as const;
 };
