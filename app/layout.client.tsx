d3334231d7c2c95bb75a9ac82afcde8ef57ab3fe
Daniil Polienko <donyaschefer@gmail.com>
2025-02-11 16:29:44 +0300

    fix: revert

--- app/layout.client.tsx
+++ app/layout.client.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import {usePathname} from 'next/navigation';
-import {useEffect} from 'react';
 
 import {Footer} from '@/components/Footer';
 import {Header} from '@/components/header/Header';
-import {useWeglot} from '@/hooks/useWeglot';
 
 import type {ReactNode} from 'react';
 
@@ -13,13 +11,13 @@ import type {ReactNode} from 'react';
  * Client layout wrapper component
  * Handles header visibility based on current path
  ********************************************************************************************/
-export function LayoutClient({children, lang}: {children: ReactNode; lang: string}): ReactNode {
+export function LayoutClient({children}: {children: ReactNode}): ReactNode {
 	const pathname = usePathname();
-	const [, setLang] = useWeglot('wg_b6fdc2a2e16175fd09ce44998516155b3', 'en');
-	useEffect(() => {
-		console.log('set:', lang);
-		setLang(lang);
-	}, [lang, pathname, setLang]);
+	// const [, setLang] = useWeglot('wg_b6fdc2a2e16175fd09ce44998516155b3', 'en');
+	// useEffect(() => {
+	// 	console.log('set:', lang);
+	// 	setLang(lang);
+	// }, [lang, pathname, setLang]);
 	return (
 		<div className={'flex flex-col px-4'}>
 			{/* <button onClick={() => setLang('en')}>{'EN'}</button>
