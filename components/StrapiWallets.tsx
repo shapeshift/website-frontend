6204bc7e938478d8168d8b70d19dad0d013ac444
Major <major@ycorpo.com>
2025-02-25 11:48:57 +0100

    feat: supported-chains page

--- components/StrapiWallets.tsx
+++ components/StrapiWallets.tsx
@@ -5,47 +5,47 @@ import Link from 'next/link';
 import {useEffect, useState} from 'react';
 
 import {cl} from '@/components/utils/cl';
-import {getSupportedWallets} from '@/components/utils/query';
+import {getSupportedChains} from '@/components/utils/query';
 
-import type {TSupportedWalletData} from '@/types/strapi';
+import type {TSupportedChainData} from '@/types/strapi';
 import type {ReactNode} from 'react';
 
 export function StrapiWallets(): ReactNode {
-	const [wallet, setWallet] = useState<TSupportedWalletData[] | null>(null);
+	const [chains, setChains] = useState<TSupportedChainData[] | null>(null);
 
 	useEffect(() => {
-		const fetchWallets = async (): Promise<void> => {
-			const wallets = await getSupportedWallets();
-			setWallet(wallets);
+		const fetchChains = async (): Promise<void> => {
+			const chains = await getSupportedChains();
+			setChains(chains);
 		};
-		fetchWallets();
+		fetchChains();
 	}, []);
 
 	return (
 		<div className={'flex w-full justify-center'}>
 			<div className={'context flex flex-col justify-center'}>
 				<div className={'mb-20 grid gap-6 md:grid-cols-2 lg:grid-cols-3'}>
-					{(wallet || []).map(wallet => {
+					{(chains || []).map(chain => {
 						return (
 							<Link
-								key={wallet.slug}
-								href={`/resources/supported-wallets/${wallet.slug}`}
+								key={chain.slug}
+								href={`/resources/supported-chains/${chain.slug}`}
 								className={cl(
 									'rounded-2xl p-6 transition-all duration-300 hover:scale-[1.02] hover:bg-secondHoverBg'
 								)}>
 								<div
-									className={'h-[204px] max-w-[408px] overflow-hidden rounded-2xl p-10'}
+									className={'h-[204px] max-w-[408px] overflow-hidden rounded-2xl px-10 pt-10'}
 									style={{
 										backgroundImage: "url('/supported-wallets/cover.png')",
 										backgroundSize: 'cover',
 										backgroundPosition: 'center'
 									}}>
-									{wallet?.featuredImg?.url ? (
+									{chain?.featuredImg?.url ? (
 										<Image
-											src={`${process.env.STRAPI_URL}${wallet?.featuredImg?.url}`}
-											alt={wallet.slug}
-											width={wallet?.featuredImg?.width ?? 0}
-											height={wallet?.featuredImg?.height ?? 0}
+											src={`${process.env.STRAPI_URL}${chain?.featuredImg?.url}`}
+											alt={chain.slug}
+											width={chain?.featuredImg?.width ?? 0}
+											height={chain?.featuredImg?.height ?? 0}
 											className={'size-full object-contain'}
 										/>
 									) : (
@@ -55,12 +55,12 @@ export function StrapiWallets(): ReactNode {
 
 								<div className={'mt-6 flex flex-col gap-2'}>
 									<div>
-										<p className={'text-2xl text-white'}>{wallet.name}</p>
+										<p className={'text-2xl text-white'}>{chain.name}</p>
 										<p
 											className={
 												'line-clamp-6 whitespace-break-spaces break-keep text-sm text-gray-500'
 											}>
-											{wallet.description}
+											{chain.description}
 										</p>
 									</div>
 								</div>
